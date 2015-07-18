1e561636fa617d05486bdb252254d10c66d20b18
loginjames <me.jamesmy@hotmail.com>
2015-07-18 09:47:18 +0700

    ajax

---
+++ js/time.js
@@ -0,0 +1,11 @@
+function getCorrectTime(){
+    var date = new Date();
+    var time = "";
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    time += ((hours<10)? ("0" + hours):hours) + ":";
+    time += ((minutes<10)? ("0" + minutes):minutes) + ":";
+    time += ((seconds<10)? ("0" + seconds):seconds);
+    return time;
+}
---
+++ php/QueryVolocity.php
@@ -0,0 +1,28 @@
+
+<?php
+	include 'connectDB.php';
+	$sql = "SELECT *
+			FROM (
+				SELECT *
+				FROM TEST_VOLOCITY
+				WHERE SQUAD_ID = 1
+				ORDER BY SPRINT_NO DESC
+				LIMIT 0,6
+				) as TEST_VOLOCITY1
+			order by SPRINT_NO;";
+	$result = $conn->query($sql);
+	$userJson1 = array();
+	$userJson2 = array();
+	$userJson3 = array();
+	$userJson4 = array();
+	$ans = array();
+    while($row = $result->fetch_assoc()) {
+	    array_push($userJson1, (int)$row["SPRINT_NO"]);
+    	array_push($userJson2, (int)$row["POINT_COMMIT"]);
+    	array_push($userJson3, (int)$row["POINT_COMPLETE"]);
+    }
+    $ans['SPRINT_NO'] = $userJson1;
+    $ans['POINT_COMMIT'] = $userJson2;
+    $ans['POINT_COMPLETE'] = $userJson3;
+    echo json_encode($ans);
+?>
