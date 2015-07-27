84486d6eb3267c0b42422e2d7f671a404d3942e0
loginjames <me.jamesmy@hotmail.com>
2015-07-27 10:55:46 +0700

    push

--- php/QueryCountCard.php
+++ php/QueryCountCard.php
@@ -2,10 +2,10 @@
 	include 'connectDB.php';
 	$sql = "SELECT COUNT(*) as COUNT
 		FROM TEST_RETRO_CARD as t1
-		WHERE t1.SQUAD_ID = 8 AND t1.SPRINT_NO >= all(
+		WHERE t1.SQUAD_ID = ".$_POST['SQUAD_ID']." AND t1.SPRINT_NO >= all(
 		SELECT t2.SPRINT_NO
 		FROM TEST_RETRO_CARD as t2
-		WHERE t2.SQUAD_ID = 8)";
+		WHERE t2.SQUAD_ID = ".$_POST['SQUAD_ID'].")";
 	$result = $con->query($sql);
 	$row = $result->fetch_assoc();
     $count = (int)$row['COUNT'];
